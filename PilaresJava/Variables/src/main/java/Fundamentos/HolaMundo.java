268bb09892b6b95bef5ff582759f2101e65ab459
jvelosa <jrvelosa@misena.edu.co>
2019-10-19 23:06:20 -0500

    Creacion de carpeta Variables

---
+++ PilaresJava/Variables/src/main/java/Fundamentos/HolaMundo.java
@@ -0,0 +1,20 @@
+package Fundamentos;
+
+public class HolaMundo {
+
+public static void main(String[] args) {
+        
+        String saludar = "saludos desde Java";
+        
+        // System.out.println(saludar);
+        //System.out.println(saludar);
+        //System.out.println(saludar);
+        
+        var despedirse = "hasta luego";
+        System.out.println(despedirse);
+        
+        var numero = 1;
+        System.out.println(numero);
+        
+    }
+}
