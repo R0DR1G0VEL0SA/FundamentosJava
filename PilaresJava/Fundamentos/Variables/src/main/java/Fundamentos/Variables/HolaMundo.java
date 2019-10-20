3a31da12cc9dbc1880ef2529fa6e5f44d84a7ff8
jvelosa <jrvelosa@misena.edu.co>
2019-10-19 22:09:50 -0500

    Primer commit : Hola Mundo en Java

---
+++ PilaresJava/Fundamentos/Variables/src/main/java/Fundamentos/Variables/HolaMundo.java
@@ -0,0 +1,21 @@
+package Fundamentos.Variables;
+
+public class HolaMundo {
+	
+	 public static void main(String[] args) {
+	        
+	        String saludar = "saludos desde Java";
+	        
+	        //System.out.println(saludar);
+	        //System.out.println(saludar);
+	        //System.out.println(saludar);
+	        
+	        var despedirse = "hasta luego";
+	        System.out.println(despedirse);
+	        
+	        var numero = 1;
+	        System.out.println(numero);
+	        
+	    }
+
+}
