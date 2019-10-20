c6b2f7b35df890d052f6e7f518e8b2ac2c32952b
jvelosa <jrvelosa@misena.edu.co>
2019-10-19 23:27:21 -0500

    Fin Ejercicios con Variables

---
+++ PilaresJava/Variables/src/main/java/Fundamentos/ReglasIdentificadores.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+public class ReglasIdentificadores {
+
+	public static void main(String[] args) {
+
+        var saludosDesdeJava = "Hola desde Java";
+        System.out.println( saludosDesdeJava );
+        
+        int _holaDesde1Java ;
+        float $adios;
+        char adios;
+        char a123dios;
+     
+    }
+}
